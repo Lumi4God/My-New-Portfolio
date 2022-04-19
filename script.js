30e06f92efb9fd17b3427699bcde4ff33a161aae
Olumide Jones <olumideyjones@yahoo.com>
2022-04-19 10:12:51 +0200

    resume about write up

--- script.js
+++ script.js
@@ -72,6 +72,7 @@ $(document).ready(function(){
     });
 });
 
+
 //////////////////////////////////////////////
 /****************Digital*clock***************/
 const message = document.querySelector("#message")
